5a8f67d4d295d4e70fac34b3fffc14da0d1e89db
RyanSchorel <ryanschorel@gmail.com>
2025-06-15 21:11:21 +0200

    logo site add
    - als je nu op het logo klikt word je naar een aparte pagina gestuurd van de website link

--- src/components/structure/Header.js
+++ src/components/structure/Header.js
@@ -1,12 +1,14 @@
 export const RenderHeader = () => {
-
      return (
           <div className="header">
                <div className="logo">
-                    <img onClick={ () => { window.location.href="https://www.uncinc.nl/nl"} } src="/unc_inc_logo.png" alt="Logo"/>
+                    <img 
+                      onClick={() => { window.open("https://www.uncinc.nl/nl", "_blank") }} 
+                      src="/unc_inc_logo.png" 
+                      alt="Logo" 
+                    />
                </div>
                <h1>Authentication Unc Inc</h1>
           </div>
      )
 }
-
