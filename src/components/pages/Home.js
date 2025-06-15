9ce9f363e26d1748917f4d44fdb0f9d0ab59a605
RyanSchorel <ryanschorel@gmail.com>
2025-06-15 18:42:17 +0200

    text add op home page
    - text over wat het project is

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,7 @@ export const Home = () => {
      return (
           <div className="page">
                <h2>Welkom op de UNC INC stageopdracht website!</h2>
-               <p>Deze website is gemaakt met React</p>
+               <p>Deze website is gemaakt met React, en de bedoeling is om de kunnen inloggen met de mee gegeven inlog gegevns, zoals het wachtwoord. Als je in logt op de website met de verkeerde gegevens hoor je een foutmelding te krijgen. Als je bent ingelogt krijg je toegang tot een /dashboard. En als je niet bent ingelogt moet je geen toegang kunnen krijgen tot de /dashboard pagina.</p>
           </div>
      )
 }
