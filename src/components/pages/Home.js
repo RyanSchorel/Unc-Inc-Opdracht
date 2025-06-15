635432e588edb93eb6263a44d6a4987a1dd4c8bc
RyanSchorel <ryanschorel@gmail.com>
2025-06-15 21:07:17 +0200

    home aangepast
    spelfouten verbetered

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,10 +4,10 @@ export const Home = () => {
           <div className="page">
                <h2>Welkom op de UNC INC stageopdracht website!</h2>
                <p>Deze website is gemaakt met React, 
-                    en de bedoeling is om de kunnen inloggen met de mee gegeven inlog gegevens, 
+                    en de bedoeling is om te kunnen inloggen met de mee gegeven inlog gegevens, 
                     zoals het wachtwoord. Als je inlogt op de website met de verkeerde gegevens hoor je een foutmelding te krijgen. 
                     Als je bent ingelogd krijg je toegang tot een /dashboard. 
-                    En als je niet bent ingelogt moet je geen toegang kunnen krijgen tot de /dashboard pagina.</p>
+                    En als je niet bent ingelogd moet je geen toegang kunnen krijgen tot de /dashboard pagina.</p>
           </div>
      )
 }
