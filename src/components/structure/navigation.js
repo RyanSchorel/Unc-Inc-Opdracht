8da24b380bc028776ea10ae413c7ed56e9f02b4d
RyanSchorel <ryanschorel@gmail.com>
2025-06-15 18:56:40 +0200

    dubbele home weg gehaald

--- src/components/structure/navigation.js
+++ src/components/structure/navigation.js
@@ -7,7 +7,6 @@ import { Dashboard } from "../pages/Dashboard"
 
 export const nav = [
      { path: "/", name: "Home", element: <Home />, isMenu: true, isPrivate: false },
-     { path: "/home", name: "Home", element: <Home />, isMenu: true, isPrivate: false },
      { path: "/login", name: "Login", element: <Login />, isMenu: false, isPrivate: false },
      { path: "/dashboard", name: "Dashboard", element: <Dashboard />, isMenu: true, isPrivate: true },
 
